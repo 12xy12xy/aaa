e2b1bc8ba2c00d2745a012ac5da137737fa9d8f5
12xy12xy <content.xexy@gmail.com>
2025-05-03 09:54:48 +0530

    Add files via upload

---
+++ search.js
@@ -0,0 +1,46 @@
+const dataList = document.getElementById('dataList');
+const searchBox = document.getElementById('searchBox');
+const clearButton = document.getElementById('clearButton');
+
+function renderList(filter) {
+    dataList.innerHTML = '';
+    let filteredData = [];
+
+    if (filter.length >= 3) {
+        filteredData = data.filter(item => 
+            item.name.toLowerCase().includes(filter.toLowerCase())
+        );
+    } else {
+        filteredData = data.filter(item => 
+            item.name.toLowerCase().split(' ').some(word => word.toLowerCase() === filter.toLowerCase())
+        );
+    }
+
+    if (filteredData.length > 0) {
+        dataList.innerHTML = filteredData.map(item => 
+            `<div><a href="${item.url}" target="_blank">${item.name}</a></div>`
+        ).join('');
+    } else {
+        dataList.innerHTML = '';
+    }
+}
+
+searchBox.addEventListener('input', function() {
+    renderList(searchBox.value);
+});
+
+searchBox.addEventListener('focus', function() {
+    clearButton.classList.add('show');
+});
+
+searchBox.addEventListener('blur', function() {
+    if (searchBox.value.length === 0) {
+        clearButton.classList.remove('show');
+    }
+});
+
+clearButton.addEventListener('click', function() {
+    searchBox.value = '';
+    renderList('');
+    clearButton.classList.remove('show');
+});
---
+++ searchdata.js
@@ -0,0 +1,5 @@
+const data = [
+            { name: 'Best laptops under 50', url: 'https://example.com/laptops-under-50000' },
+            { name: 'Best laptops under 3', url: 'https://example.com/laptops-under-30000' },
+            { name: 'Best gaming laptops under 70000', url: 'https://example.com/gaming-laptops-under-70000' }
+        ];
