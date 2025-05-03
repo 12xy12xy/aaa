791387c7170e599f5cae1f712704f2d8797da17c
12xy12xy <content.xexy@gmail.com>
2025-05-03 11:58:04 +0530

    Update searchdata.js

--- searchdata.js
+++ searchdata.js
@@ -1,5 +1,6 @@
-const data = [
-            { name: 'Best laptops under 50', url: 'https://example.com/laptops-under-50000' },
-            { name: 'Best laptops under 3', url: 'https://example.com/laptops-under-30000' },
-            { name: 'Best gaming laptops under 70000', url: 'https://example.com/gaming-laptops-under-70000' }
-        ];
+let searchData = [
+  { name: 'Best laptops under 50000', url: 'https://example.com/laptops-under-50000' },
+  { name: 'Best laptops under 30000', url: 'https://example.com/laptops-under-30000' },
+  { name: 'Best gaming laptops under 70000', url: 'https://example.com/gaming-laptops-under-70000' }
+];
+window.data = searchData;
